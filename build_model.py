2f9682495c8385b6ce3862059f48c7da046ad4f4
soma-y1029 <57809819+soma-y1029@users.noreply.github.com>
2020-10-23 20:00:36 -0700

    improved lemmatization.

--- build_model.py
+++ build_model.py
@@ -1,44 +1,56 @@
-from spacy.lang.en import English, stop_words
+import re
+import spacy
+import string
+
 from nltk.corpus import twitter_samples
-import nltk, re
+
+SPACY_DIR = 'en'
 
 
 class Model():
     def __init__(self):
-        self.__model_nlp = English()
+        """
+        Constructor
+        """
+        self.__model_nlp = spacy.load(SPACY_DIR)
 
-        self.__tokenizer = self.__model_nlp.Defaults.create_tokenizer(self.__model_nlp)
-        res = self.__get_labeled_tweets('positive_tweets.json')
-        print(res)
+        pos_sample = self.__get_labeled_tweets('positive_tweets.json')
+        neg_sample = self.__get_labeled_tweets('negative_tweets.json')
 
     def __get_labeled_tweets(self, file_name):
+        """
+        Get labeled tweets from nltk
+        :param file_name: name of file in string
+        :return: cleaned list of lists that contain tokens for each tweets
+        """
         raw_tweets = [' '.join(tokens) for tokens in twitter_samples.tokenized(file_name)[:10]]
         tokenized_tweets = []
 
         # obtain tokenizer object for each tweets from raw_tweets
         for tweet in raw_tweets:
-            # tokenized_tweets.append(self.__tokenizer(tweet))
             tokenized_tweets.append(self.__model_nlp(tweet))
 
         cleaned_list = []
         # for each tokenized tweet, perform operations
         for tweet in tokenized_tweets:
-            cleaned_tweet_tokens = []
+            cleaned_tweet_tokens = [] # temporarily store filtered tokens for tweet
             for token in tweet:
-                if self.__is_surplus(token):
+                if self.__is_surplus(token): # skip if this is surplus
                     continue
+                # append only if token is not stop words
                 if not token.is_stop:
                     cleaned_tweet_tokens.append(token.lemma_.lower())
             cleaned_list.append(cleaned_tweet_tokens)
 
         return cleaned_list
 
+
     def __is_surplus(self, token):
-        '''
+        """
         check if given token is surplus or not
         :param token: Doc object from tokenizer
         :return: True if this token is surplus
-        '''
+        """
         # remove mention @ from tokens
         mention_pattern = r'(@\w+)|(#)'
         if re.match(mention_pattern, token.text):
@@ -49,9 +61,11 @@ class Model():
         if re.match(link_pattern, token.text):
             return True
 
+        if token.text in string.punctuation:
+            return True
+
         return False
 
 
-
-
+# for testing
 model = Model()
