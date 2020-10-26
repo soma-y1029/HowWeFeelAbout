c456cb15aba5abe5e707d33bae24cf3fcfe508b5
soma-y1029 <57809819+soma-y1029@users.noreply.github.com>
2020-10-25 21:17:14 -0700

    django setup

--- how_we_fell_about/urls.py
+++ how_we_fell_about/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from how_we_fell_about import
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
 ]
